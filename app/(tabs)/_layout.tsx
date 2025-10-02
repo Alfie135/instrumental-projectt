6ee83e73191426b9649430f620389abc448b6890
Alfie135 <182797639+Alfie135@users.noreply.github.com>
2025-10-02 16:41:07 +0100

    a
    a

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,8 +1,18 @@
+import useOrientation from '@/hooks/useOrientation';
 import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
 import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 import { Tabs } from "expo-router";
+import { Orientation } from 'expo-screen-orientation';
+import { Text } from 'react-native';
 
 export default function TabLayout() {
+    const screenOrientation = useOrientation();
+      if (screenOrientation === Orientation.PORTRAIT_UP) 
+        return <Text>Rotate Device to Landscape</Text>;
+      if (screenOrientation === Orientation.PORTRAIT_DOWN)
+        return <Text>Rotate Device to Landscape</Text>;
+      if(screenOrientation === Orientation.UNKNOWN)
+        return <Text>Rotate Device to Landscape</Text>
     return (
         <Tabs
             screenOptions={{tabBarActiveTintColor: '#03fc56ff'}}>
