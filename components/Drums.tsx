bb274853ab8cac871961c8cf7eb963de8684084d
Alfie135 <182797639+Alfie135@users.noreply.github.com>
2025-10-14 14:08:14 +0100

    a
    a

--- components/Drums.tsx
+++ components/Drums.tsx
@@ -1,7 +1,10 @@
+import { loadAudioPlayers } from "@/helpers/audio";
 import React from "react";
 import { Text, View } from "react-native";
 
 export default function Drums() {
+    const players = loadAudioPlayers();
+    
     return <View> 
         <Text>
             Drums Component
