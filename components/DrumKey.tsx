05a422a6b29664d8a1f2c7a58f31691849b8eb76
Alfie135 <182797639+Alfie135@users.noreply.github.com>
2025-10-23 15:24:15 +0100

    a
    a

--- components/DrumKey.tsx
+++ components/DrumKey.tsx
@@ -1,16 +1,18 @@
 import { AudioPlayer } from "expo-audio";
-import { Pressable, StyleSheet, View } from "react-native";
+import { Pressable, StyleSheet } from "react-native";
 
 interface DrumKeyProps {
   player: AudioPlayer,
   top: number,
   left: number,
-  //radius: number,
+  radius: number,
+  height: number,
+  width: number
 }
 
 export default function WhiteKey(props: DrumKeyProps) {
 
-  const {player, top, left}= props;
+  const {player, top, left, radius, height, width}= props;
   const onPress = () => {
     player.seekTo(0); //Seek to time 0 of audio file
     player.play(); //Play
@@ -18,17 +20,17 @@ export default function WhiteKey(props: DrumKeyProps) {
 
     return (
       <Pressable onPress={onPress} // When the button is pressed, change colour to colour listed:
-        style = {({pressed}) => [{ backgroundColor: pressed ? "rgba(165, 170, 180, 1)" : "white", height: 100, top:top, left:left, opacity: 0.02, borderRadius: 50 }]}>
-        <View style = {styles.DrumKey}></View>
+        style = {({pressed}) => [{ backgroundColor: pressed ? "rgba(165, 170, 180, 1)" : "white", top:top, left:left, opacity: 0.05, borderRadius:radius, width:width, height:height}]}>
+        {/* <View style = {styles.DrumKey}></View> */}
       </Pressable>
     );  
 }
 
 const styles = StyleSheet.create({ // Formatting for the pressable buttons
   DrumKey: {
-    width: 100,
-    height: 100,
-    borderRadius: 50,
+    width: 50,
+    height: 50,
+    borderRadius: 25,
     borderWidth: 1.5,
     borderColor: "black",
   },
--- components/Drums.tsx
+++ components/Drums.tsx
@@ -10,13 +10,13 @@ export default function Drums() {
 
     return <View style ={styles.mainview}> 
         <ImageBackground source={image} style ={styles.imageStyling}> 
-            <DrumKey player={players.snare} top={165} left={46}/>
-            <DrumKey player={players.hihat} top={65} left={-2}/>
-            <DrumKey player={players.crashCymbal} top={160} left={-55}/>
-            <DrumKey player={players.smallTom} top={70} left={-105}/>
-            <DrumKey player={players.mediumTom} top={80} left={-110}/>
-            <DrumKey player={players.rideCymbal} top={70} left={-110}/>
-            <DrumKey player={players.floorTom} top={160} left={-245}/>
+            <DrumKey player={players.snare} top={163} left={47} height={106} width={106}radius={53}/>
+            <DrumKey player={players.hihat} top={60} left={-10} height={106} width={106}radius={53}/>
+            <DrumKey player={players.crashCymbal} top={164} left={-61} height={86} width={86}radius={43}/>
+            <DrumKey player={players.smallTom} top={95} left={-88} height={66} width={66}radius={33}/>
+            <DrumKey player={players.mediumTom} top={95} left={-75} height={80} width={80}radius={40}/>
+            <DrumKey player={players.rideCymbal} top={62} left={-65} height={112} width={112}radius={56}/>
+            <DrumKey player={players.floorTom} top={168} left={-196} height={84} width={84}radius={42}/>
         </ImageBackground>
         <Text>Press on the drum to play its corresponding sound! </Text>
     </View>
